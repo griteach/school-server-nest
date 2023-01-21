827182426805618ec159727706cb754ebe92a562
griteach <griteach22@gmail.com>
2023-01-21 22:54:29 +0900

    testing...

--- src/movie/movie.service.spec.ts
+++ src/movie/movie.service.spec.ts
@@ -15,4 +15,27 @@ describe('MovieService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('getAll()', () => {
+    it('should return an array', () => {
+      //service를 통해서 getAll에 접근할 수 있다.
+      const result = service.getAll();
+
+      //익스펙트 : 기대되는 값,
+      //투 비 인스턴스 오브 어레이 = 어레이 인스턴스가 되어야 한다.
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne()', () => {
+    it('getOne', () => {
+      service.create({
+        title: 'Test',
+        year: 2022,
+        genres: ['humor', 'horor'],
+      });
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+    });
+  });
 });
