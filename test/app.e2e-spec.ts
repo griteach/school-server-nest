68a6f346e80c553f2b29a7fd83344cc48087af77
griteach <griteach22@gmail.com>
2023-01-22 23:34:53 +0900

    e2e test fin

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
     await app.init();
   });
 
@@ -19,6 +26,50 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('This is NEST JS HOME.');
+  });
+
+  describe('/movie', () => {
+    it('GET', () => {
+      return request(app.getHttpServer()).get('/movie').expect(200).expect([]);
+    });
+    it('POST', () => {
+      return request(app.getHttpServer())
+        .post('/movie')
+        .send({
+          title: 'jaden',
+          year: 2017,
+          genres: ['test'],
+        })
+        .expect(201);
+    });
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movie')
+        .send({
+          title: 'jaden',
+          year: 2017,
+          genres: ['test'],
+          other: 'thing',
+        })
+        .expect(400);
+    });
+    it('DELETE', () => {
+      return request(app.getHttpServer()).delete('/movie').expect(404);
+    });
+  });
+  describe('/movie/:id', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer()).get('/movie/1').expect(200);
+    });
+    it('PATCH 200', () => {
+      return request(app.getHttpServer())
+        .patch('/movie/1')
+        .send({ title: 'Yehwan' })
+        .expect(200);
+    });
+    it('DELETE 200', () => {
+      return request(app.getHttpServer()).delete('/movie/1').expect(200);
+    });
   });
 });
